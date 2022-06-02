0737cce00afc2abaa6d5a8d63833909a7b16c5db
Truong <quangtruong01234@gmail.com>
2022-06-02 13:02:08 +0700

    second commit

--- front-end/src/routes/router.js
+++ front-end/src/routes/router.js
@@ -2,8 +2,10 @@ import Home from '../pages/home/Home';
 import Error from '../pages/error/Error';
 import ResetPassword from '../pages/resetPassword/ResetPassword';
 import NewPassword from '../pages/newPassword/NewPassword';
+import UserCompany from '../pages/userCompany/UserCompany';
 
 const publicRoutes = [
+    {path: '/UserCompany', component: UserCompany},
     {path: '/resetPassword', component: ResetPassword},
     {path: '/NewPassword', component: NewPassword},
     { path: '/', component: Home },
