b7514a8bea367eacbba5fec24f416778759273c8
unknown <bcd20144@gmail.com>
2022-06-06 01:05:01 +0700

    Create input skeleton by CongDanh

---
+++ front-end/src/components/input/Input.jsx
@@ -0,0 +1,33 @@
+import { IoIosSearch, IoIosClose } from 'react-icons/io';
+
+const Input = ({
+    width,
+    height,
+    label,
+    placeholder,
+    type,
+    name,
+    fontSize,
+    icon,
+    className,
+}) => {
+    return (
+        <label className="block" style={{ width: width }}>
+            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                {label}
+            </span>
+            <input
+                type={type}
+                name={name}
+                style={{ height: height || 45, fontSize: fontSize }}
+                className={
+                    'mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 font-medium focus:outline-none focus:border-purple-700 block w-full rounded-md sm:text-sm focus:ring-1 ' +
+                    className
+                }
+                placeholder={placeholder}
+            />
+        </label>
+    );
+};
+
+export default Input;
