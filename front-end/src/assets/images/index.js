f8870e6dc869b155cacc131783f910fece7ee584
unknown <bcd20144@gmail.com>
2022-06-07 09:58:00 +0700

    Create address model for backend

--- front-end/src/assets/images/index.js
+++ front-end/src/assets/images/index.js
@@ -1,6 +1,7 @@
 const images = {
     certificate1: require('./certification1.svg').default,
     certificate2: require('./certification2.svg').default,
+    homeImage: require('./homeImage.svg').default,
 };
 
 export default images;
