149219bb34e30dd9bf7244d337634bde20906778
nghia123vn <123vn2000@gmail.com>
2022-07-05 15:20:46 +0700

    list company using promises

--- back-end/src/routes/company.js
+++ back-end/src/routes/company.js
@@ -4,4 +4,5 @@ const Token = require('../app/controllers/token');
 
 router.post('/create', companyController.create);
 
+router.get('/list/:page', companyController.listCompany);
 module.exports = router;
