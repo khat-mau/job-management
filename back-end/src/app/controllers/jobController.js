d4ce99322560bf7e983d193b5ebdb4a34ebcfffe
nghia123vn <123vn2000@gmail.com>
2022-07-06 12:04:27 +0700

    list job

--- back-end/src/app/controllers/jobController.js
+++ back-end/src/app/controllers/jobController.js
@@ -23,6 +23,26 @@ class jobController {
             });
         }
     }
+
+    async listJob(req, res) {
+        try {
+            let perpage = 6;
+            let page = req.params.page || 1;
+            let listJob;
+            let listJobWasFilter;
+            const promises1 = new Promise(resolve => resolve( Job.countDocuments({})));
+            const promises2 = new Promise(resolve => resolve( Job.find().skip((perpage*page)-perpage).limit(perpage)));
+            await Promise.all([promises1, promises2]).then(([result1, result2]) => {listJob = result1; listJobWasFilter = result2;});
+            const toltalPage = listJob%perpage === 0 ? listJob/perpage : parseInt(listJob/ perpage)+1;
+            res.status(200).json({ errorStatus: false,data: {page, toltalPage, listJobWasFilter}});
+        }catch(e) {
+            res.status(500).json({
+                errorStatus: true,
+                message: 'find job failed',
+                e,
+            });
+        }
+    }
 }
 
 module.exports = new jobController();
--- back-end/src/routes/job.js
+++ back-end/src/routes/job.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const jobController = require('../app/controllers/jobController');
 
 router.post('/create', jobController.create);
-
+router.get('/list/:page', jobController.listJob);
 module.exports = router;
